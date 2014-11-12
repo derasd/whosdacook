c3d90107f33bedb82e47189ef3d3bc114389640f
derasd <buenascold@gmail.com>
2014-11-12 10:48:34 +0100

    new version 0.3

--- gen/com/derasd/whosdacook/R.java
+++ gen/com/derasd/whosdacook/R.java
@@ -32,31 +32,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0013;
-        public static final int btCalendar=0x7f0a0010;
+        public static final int action_settings=0x7f0a0016;
         public static final int btSend=0x7f0a000b;
-        public static final int btSetInput=0x7f0a000f;
-        public static final int btSync=0x7f0a0011;
-        public static final int calData=0x7f0a000e;
-        public static final int cb16=0x7f0a0002;
-        public static final int cb17=0x7f0a0007;
-        public static final int cb18=0x7f0a0006;
-        public static final int cb19=0x7f0a0005;
+        public static final int cb16=0x7f0a0000;
+        public static final int cb17=0x7f0a0001;
+        public static final int cb18=0x7f0a0002;
+        public static final int cb19=0x7f0a0003;
         public static final int cb20=0x7f0a0004;
-        public static final int cb21=0x7f0a0003;
-        public static final int cb22=0x7f0a0000;
-        public static final int cbArrive30=0x7f0a0009;
-        public static final int cbCook=0x7f0a0001;
-        public static final int cbEat=0x7f0a000a;
-        public static final int cbLeave30=0x7f0a0008;
-        public static final int cbNot=0x7f0a000d;
-        public static final int cbShowAll=0x7f0a0012;
-        public static final int menu_settings=0x7f0a0014;
-        public static final int tfProp=0x7f0a000c;
+        public static final int cb21=0x7f0a0005;
+        public static final int cb22=0x7f0a0006;
+        public static final int cbArrive15=0x7f0a000a;
+        public static final int cbArrive30=0x7f0a0008;
+        public static final int cbLeave15=0x7f0a0009;
+        public static final int cbLeave30=0x7f0a000c;
+        public static final int cbPlusOne=0x7f0a0012;
+        public static final int line=0x7f0a000d;
+        public static final int menu_settings=0x7f0a0017;
+        public static final int radioGroup1=0x7f0a000e;
+        public static final int radioGroup2=0x7f0a0013;
+        public static final int rbCook=0x7f0a0010;
+        public static final int rbCookJoin=0x7f0a0011;
+        public static final int rbJoin=0x7f0a000f;
+        public static final int rbLater=0x7f0a0014;
+        public static final int rbNot=0x7f0a0015;
+        public static final int tfProp=0x7f0a0007;
     }
     public static final class layout {
-        public static final int activity_input=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_main=0x7f030000;
     }
     public static final class menu {
         public static final int input=0x7f090000;
@@ -74,59 +76,65 @@ public final class R {
         public static final int _22_00=0x7f070008;
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
-        public static final int arrive30=0x7f07000f;
-        public static final int calendar=0x7f070014;
-        public static final int kochen=0x7f070003;
-        public static final int leave30=0x7f070010;
-        public static final int menu_settings=0x7f070017;
-        public static final int mitessen=0x7f070002;
-        public static final int need_nothing=0x7f070013;
-        public static final int pref_alarm_setting=0x7f070020;
-        public static final int pref_alert_email=0x7f070019;
-        public static final int pref_alert_email_summary=0x7f07001a;
-        public static final int pref_alert_pop=0x7f07001b;
-        public static final int pref_alert_pop_summary=0x7f07001c;
-        public static final int pref_cal_name=0x7f070023;
-        public static final int pref_cal_name_summary=0x7f070024;
-        public static final int pref_cal_owner=0x7f070025;
-        public static final int pref_cal_owner_summary=0x7f070026;
-        public static final int pref_cal_type=0x7f070027;
-        public static final int pref_cal_type_summary=0x7f070028;
-        public static final int pref_default_display_name=0x7f07002e;
-        public static final int pref_description_social_recommendations=0x7f07002c;
+        public static final int arrive15=0x7f07000f;
+        public static final int arrive30=0x7f070010;
+        public static final int calendar=0x7f070019;
+        public static final int cook=0x7f070003;
+        public static final int cookOrJoin=0x7f070018;
+        public static final int input_activity_label=0x7f07002e;
+        public static final int join=0x7f070002;
+        public static final int later=0x7f070015;
+        public static final int leave15=0x7f070011;
+        public static final int leave30=0x7f070012;
+        public static final int menu_settings=0x7f07001c;
+        public static final int not_attending=0x7f070017;
+        public static final int plusOne=0x7f070016;
+        public static final int pref_alarm_setting=0x7f070025;
+        public static final int pref_alert_email=0x7f07001e;
+        public static final int pref_alert_email_summary=0x7f07001f;
+        public static final int pref_alert_pop=0x7f070020;
+        public static final int pref_alert_pop_summary=0x7f070021;
+        public static final int pref_cal_name=0x7f070028;
+        public static final int pref_cal_name_summary=0x7f070029;
+        public static final int pref_cal_owner=0x7f07002a;
+        public static final int pref_cal_owner_summary=0x7f07002b;
+        public static final int pref_cal_type=0x7f07002c;
+        public static final int pref_cal_type_summary=0x7f07002d;
+        public static final int pref_default_display_name=0x7f070034;
+        public static final int pref_description_social_recommendations=0x7f070032;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f070030;
+        public static final int pref_header_data_sync=0x7f070036;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f07002a;
+        public static final int pref_header_general=0x7f070030;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f070033;
-        public static final int pref_ringtone_silent=0x7f070036;
-        public static final int pref_sync_frequency=0x7f07001d;
-        public static final int pref_sync_frequency_summary=0x7f07001e;
-        public static final int pref_title_add_friends_to_messages=0x7f07002f;
-        public static final int pref_title_display_name=0x7f07002d;
-        public static final int pref_title_new_message_notifications=0x7f070034;
-        public static final int pref_title_ringtone=0x7f070035;
-        public static final int pref_title_social_recommendations=0x7f07002b;
-        public static final int pref_title_sync_frequency=0x7f070031;
-        public static final int pref_title_system_sync_settings=0x7f070032;
-        public static final int pref_title_vibrate=0x7f070037;
-        public static final int pref_user_name=0x7f070021;
-        public static final int pref_user_name_summary=0x7f070022;
-        public static final int pref_user_profile=0x7f07001f;
-        public static final int proposal=0x7f070011;
-        public static final int send=0x7f070012;
+        public static final int pref_header_notifications=0x7f070039;
+        public static final int pref_ringtone_silent=0x7f07003c;
+        public static final int pref_sync_frequency=0x7f070022;
+        public static final int pref_sync_frequency_summary=0x7f070023;
+        public static final int pref_title_add_friends_to_messages=0x7f070035;
+        public static final int pref_title_display_name=0x7f070033;
+        public static final int pref_title_new_message_notifications=0x7f07003a;
+        public static final int pref_title_ringtone=0x7f07003b;
+        public static final int pref_title_social_recommendations=0x7f070031;
+        public static final int pref_title_sync_frequency=0x7f070037;
+        public static final int pref_title_system_sync_settings=0x7f070038;
+        public static final int pref_title_vibrate=0x7f07003d;
+        public static final int pref_user_name=0x7f070026;
+        public static final int pref_user_name_summary=0x7f070027;
+        public static final int pref_user_profile=0x7f070024;
+        public static final int proposal=0x7f070013;
+        public static final int send=0x7f070014;
         public static final int setInput=0x7f070007;
-        public static final int show_all_calendar=0x7f070015;
+        public static final int show_all_calendar=0x7f07001a;
         public static final int sync=0x7f070004;
         public static final int title_activity_input=0x7f070006;
-        public static final int title_activity_main=0x7f070018;
-        public static final int title_activity_menu=0x7f070016;
-        public static final int title_activity_settings=0x7f070029;
+        public static final int title_activity_main=0x7f07001d;
+        public static final int title_activity_menu=0x7f07001b;
+        public static final int title_activity_settings=0x7f07002f;
         public static final int zuHause=0x7f070005;
     }
     public static final class style {
--- src/com/derasd/whosdacook/MainActivity.java
+++ src/com/derasd/whosdacook/MainActivity.java
@@ -2,58 +2,362 @@ package com.derasd.whosdacook;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.CalendarContract;
 import android.provider.CalendarContract.Calendars;
+import android.provider.CalendarContract.Events;
 import android.provider.CalendarContract.Instances;
+import android.provider.CalendarContract.Reminders;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	String calName = "";
-	String calType = "";
-	String calOwner = "";
-
-	
-	private Cursor cur = null;
 	public static final String DEBUG_TAG = "com.derasd.whosdacook";
 
 	private static final int RESULT_SETTINGS = 1; // is this needed?
 
+	private String calTitle = "";
+	private String calPlusOne = "";
+	private String tfProp = "";
+	private String calName = "";
+	private String calType = "";
+	private String calOwner = "";
+	private String calEntry = "";
+
+	private boolean alertEmail;
+	private boolean alertPop;
+	private long startMillis = 0;
+	private long endMillis = 0;
+	private int startHour = 0;
+	private int startMin = 0;
+	private int endHour = 23; // if no endhour is chosen asume we stay all night
+	private int endMin = 0;
+	private boolean startSet = false;
+
 	// Array projection with calendar details
-	public static final String[] INSTANCE_PROJECTION = new String[] {
-			Calendars._ID, // 0
+	public static final String[] INSTANCE_PROJECTION = new String[] { Calendars._ID, // 0
 			Instances.BEGIN, // 1
 			Instances.END, // 2
 			Instances.TITLE, // 3
 			Instances.DESCRIPTION // 4
 	};
 
+	// Projection array. Creating indices for this array instead of doing
+	// dynamic lookups improves performance.
+	public static final String[] EVENT_PROJECTION = new String[] { Calendars._ID, // 0
+	};
+
 	// The indices for the projection array above.
 	private static final int PROJECTION_ID_INDEX = 0;
-	private static final int PROJECTION_BEGIN_INDEX = 1;
-	private static final int PROJECTION_END_INDEX = 2;
-	private static final int PROJECTION_TITLE_INDEX = 3;
-	private static final int PROJECTION_DESCRIPTION_INDEX = 4;
+
+	String[] array = { "Reis", "Nudeln", "Spaghetti", "Hamburger", "Roesti", "Thai Curry", "Ravioli", "Eight", "Nine", "Ten" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setupActionBar(); // Show the Up button in the action bar.
+		addListenerOnButton(); // add button listener
+
+		// Adds autocomplete to the menu proposal field
+		AutoCompleteTextView autoTV = (AutoCompleteTextView) findViewById(R.id.tfProp);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, array);
+		autoTV.setThreshold(1);
+		autoTV.setAdapter(adapter);
+	}
+
+	private void getPreferences() {
+
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+		calTitle = sharedPrefs.getString("prefUsername", "NULL");
+
+		alertEmail = sharedPrefs.getBoolean("prefAlertEmail", false);
+		alertPop = sharedPrefs.getBoolean("prefAlertPop", false);
+
+		calName = sharedPrefs.getString("prefCalName", "NULL");
+		calType = sharedPrefs.getString("prefCalType", "NULL");
+		calOwner = sharedPrefs.getString("prefCalOwner", "NULL");
+
+		// Remove spaces and convert to lower case
+		calName = calName.replaceAll("\\s+$", "");
+		calName = calName.toLowerCase();
+		calType = calType.replaceAll("\\s+$", "");
+		calType = calType.toLowerCase();
+		calOwner = calOwner.replaceAll("\\s+$", "");
+		calOwner = calOwner.toLowerCase();
+
+	}
+
+	/** Defines start and endtime according to the checked checkboxes */
+	public void addListenerOnButton() {
+
+		final RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
+		final RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+
+		rg1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// checkedId is the RadioButton selected
+
+				if (checkedId == R.id.rbCook) {
+					calEntry = "I'll cook!";
+					rg2.clearCheck();
+				} else if (checkedId == R.id.rbJoin) {
+					calEntry = "I'll join!";
+					rg2.clearCheck();
+				} else if (checkedId == R.id.rbCookJoin) {
+					calEntry = "I'll cook/join!";
+					rg2.clearCheck();
+
+				}
+			}
+		});
+
+		rg2.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// checkedId is the RadioButton selected
+				if (checkedId == R.id.rbNot) {
+					calEntry = "Not attending!";
+					rg1.clearCheck();
+				} else if (checkedId == R.id.rbLater) {
+					calEntry = "I'll eat later!";
+					rg1.clearCheck();
+
+				}
+			}
+		});
+
+		Button btSend = (Button) findViewById(R.id.btSend);
+		btSend.setOnClickListener(new OnClickListener() {
+			CheckBox cb16 = (CheckBox) findViewById(R.id.cb16);
+			CheckBox cb17 = (CheckBox) findViewById(R.id.cb17);
+			CheckBox cb18 = (CheckBox) findViewById(R.id.cb18);
+			CheckBox cb19 = (CheckBox) findViewById(R.id.cb19);
+			CheckBox cb20 = (CheckBox) findViewById(R.id.cb20);
+			CheckBox cb21 = (CheckBox) findViewById(R.id.cb21);
+			CheckBox cb22 = (CheckBox) findViewById(R.id.cb22);
+			CheckBox cbLeave15 = (CheckBox) findViewById(R.id.cbLeave15);
+			CheckBox cbLeave30 = (CheckBox) findViewById(R.id.cbLeave30);
+			CheckBox cbArrive15 = (CheckBox) findViewById(R.id.cbArrive15);
+			CheckBox cbArrive30 = (CheckBox) findViewById(R.id.cbArrive30);
+			CheckBox cbPlusOne = (CheckBox) findViewById(R.id.cbPlusOne);
+
+
+			@Override
+			public void onClick(View view) {
+				startSet = false;
+
+				if (cb16.isChecked() && !startSet) {
+					startHour = 16;
+					startSet = true;
+				} else if (cb16.isChecked() && startSet) {
+					endHour = 16;
+				}
+				if (cb17.isChecked() && !startSet) {
+					startHour = 17;
+					startSet = true;
+				} else if (cb17.isChecked() && startSet) {
+					endHour = 17;
+				}
+				if (cb18.isChecked() && !startSet) {
+					startHour = 18;
+					startSet = true;
+				} else if (cb18.isChecked() && startSet) {
+					endHour = 17;
+				}
+				if (cb19.isChecked() && !startSet) {
+					startHour = 19;
+					startSet = true;
+				} else if (cb19.isChecked() && startSet) {
+					endHour = 19;
+				}
+				if (cb20.isChecked() && !startSet) {
+					startHour = 20;
+					startSet = true;
+				} else if (cb20.isChecked() && startSet) {
+					endHour = 20;
+				}
+				if (cb21.isChecked() && !startSet) {
+					startHour = 21;
+					startSet = true;
+				} else if (cb21.isChecked() && startSet) {
+					endHour = 21;
+				}
+				if (cb22.isChecked() && !startSet) {
+					startHour = 22;
+					startSet = true;
+				} else if (cb22.isChecked() && startSet) {
+					endHour = 22;
+				}
+				if (cbArrive15.isChecked() && cbArrive30.isChecked()) {
+					startMin = 45;
+				} else if (cbArrive15.isChecked()) {
+					startMin = 15;
+				} else if (cbArrive30.isChecked()) {
+					startMin = 30;
+				} else if (!cbArrive30.isChecked() && !cbArrive15.isChecked()) {
+					startMin = 00;
+				}
+
+				if (cbLeave15.isChecked() && cbLeave30.isChecked()) {
+					endMin = 45;
+					if (endHour == 23) {
+						endHour = startHour;
+					}
+				} else if (cbLeave15.isChecked()) {
+					endMin = 15;
+					if (endHour == 23) {
+						endHour = startHour;
+					}
+				} else if (cbLeave30.isChecked()) {
+					endMin = 30;
+					if (endHour == 23) {
+						endHour = startHour;
+					}
+				} else if (!cbLeave30.isChecked() && !cbLeave15.isChecked()) {
+					endMin = 00;
+				}
+				if (cbPlusOne.isChecked()) {
+					calPlusOne = "+1 ";
+				}
+				readProp();
+				getStartEndTime();
+				getPreferences();
+				createEvent();
+
+			}
+		});
+	}
+
+	/** Reads the menu proposal field */
+	private void readProp() {
+
+		EditText text = (EditText) findViewById(R.id.tfProp);
+		tfProp = text.getText().toString();
+		Log.i(MainActivity.DEBUG_TAG, "Proposal:" + tfProp);
+	}
+
+	/** Calculates start and endtime and shows a toast */
+	private void getStartEndTime() {
+		Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int date = c.get(Calendar.DATE);
+
+		Calendar beginTime = Calendar.getInstance();
+		beginTime.set(year, month, date, startHour, startMin);
+		startMillis = beginTime.getTimeInMillis();
+		Calendar endTime = Calendar.getInstance();
+		endTime.set(year, month, date, endHour, endMin);
+		endMillis = endTime.getTimeInMillis();
+
+		// Change format for display
+		Calendar calBegin = Calendar.getInstance();
+		calBegin.setTimeInMillis(startMillis);
+		Calendar calEnd = Calendar.getInstance();
+		calEnd.setTimeInMillis(endMillis);
+
+		SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+		String eventStart = formatter.format(calBegin.getTime());
+		String eventEnd = formatter.format(calEnd.getTime());
+
+		Log.i(MainActivity.DEBUG_TAG, "Time: " + year + "," + month + "," + date + ";" + ": " + eventStart + " - " + eventEnd);
+
+		// Show a Toast
+		Context context = getApplicationContext();
+		CharSequence text = eventStart + " - " + eventEnd + ": " + calPlusOne + calEntry + " " + tfProp;
+		int duration = Toast.LENGTH_LONG;
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	}
+
+	public void createEvent() {
+
+		long calID = 0;
+		// Run query
+		Cursor cur = null;
+		ContentResolver cr = getContentResolver();
+		Uri uri = Calendars.CONTENT_URI;
+		String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND (" + Calendars.ACCOUNT_TYPE + " = ?) AND (" + Calendars.OWNER_ACCOUNT + " = ?))";
+		String[] selectionArgs = new String[] { calName, calType, calOwner };
+		Log.i(DEBUG_TAG, "uri: " + uri);
+		// Submit the query and get a Cursor object back.
+		cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+		Log.i(DEBUG_TAG, "Cur : " + cur);
+		// Use the cursor to step through the returned records
+		while (cur.moveToNext()) {
+			calID = cur.getLong(PROJECTION_ID_INDEX);
+			Log.i(DEBUG_TAG, "CalID 123: " + calID);
+
+		}
+
+		// ContentResolver cr = getContentResolver();
+		ContentValues values = new ContentValues();
+		values.put(Events.DTSTART, startMillis);
+		values.put(Events.DTEND, endMillis);
+		values.put(Events.TITLE, calTitle + ": " + calPlusOne + calEntry + " " + tfProp);
+		values.put(Events.CALENDAR_ID, calID);
+		values.put(Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+		Log.i(DEBUG_TAG, "Timezone retrieved=>" + TimeZone.getDefault().getID());
+		// values.put(Events.EVENT_TIMEZONE, "America/Los_Angeles");
+		uri = cr.insert(Events.CONTENT_URI, values);
+
+		Log.w(DEBUG_TAG, "CalID: " + calID);
+
+		// get the event ID that is the last element in the Uri
+		long eventID = Long.parseLong(uri.getLastPathSegment());
+
+		// Add reminder
+		// Calculate reminder time, CurrentTime-EventTime-1minute
+		long time = System.currentTimeMillis();
+		int selectedReminderValue = ((int) ((startMillis - time) / 60000)) - 1;
+		Log.w(MainActivity.DEBUG_TAG, "Time until Alarm: " + selectedReminderValue + " min");
+
+		if (alertPop) {
+			ContentValues reminder1 = new ContentValues();
+			reminder1.put(Reminders.EVENT_ID, eventID);
+			reminder1.put(Reminders.METHOD, Reminders.METHOD_ALERT);
+			reminder1.put(Reminders.MINUTES, selectedReminderValue);
+			Uri uri2 = cr.insert(Reminders.CONTENT_URI, reminder1);
+		}
+		if (alertEmail) {
+			ContentValues reminder2 = new ContentValues();
+			reminder2.put(Reminders.EVENT_ID, eventID);
+			reminder2.put(Reminders.METHOD, Reminders.METHOD_EMAIL);
+			reminder2.put(Reminders.MINUTES, selectedReminderValue);
+			Uri uri3 = cr.insert(Reminders.CONTENT_URI, reminder2);
+		}
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 	}
 
@@ -76,115 +380,4 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	/** Called when the user clicks the Send button */
-	public void setInput(View view) {
-		Intent intent = new Intent(this, SetInputActivity.class);
-		startActivity(intent);
-
-	}
-
-	private void getCalNames() {
-		SharedPreferences sharedPrefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		calName = sharedPrefs.getString("prefCalName", "NULL");
-		calType = sharedPrefs.getString("prefCalType", "NULL");
-		calOwner = sharedPrefs.getString("prefCalOwner", "NULL");
-	}
-
-	/** Opens the default calendar */
-	public void openCal(View view) {
-
-		long startMillis = 0;
-		Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
-		builder.appendPath("time");
-		ContentUris.appendId(builder, startMillis);
-		Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
-		startActivity(intent);
-	}
-
-	/** Grabs todays calendar events and displays them */
-	public void showCalEvents(View view) {
-		getCalNames();
-
-		TextView calData = (TextView) findViewById(R.id.calData);
-		calData.setSingleLine(false);
-		StringBuilder sb = new StringBuilder();
-		String details = "";
-
-		Calendar c = Calendar.getInstance();
-		int year = c.get(Calendar.YEAR);
-		int month = c.get(Calendar.MONTH);
-		int date = c.get(Calendar.DATE);
-
-		Calendar beginTime = Calendar.getInstance();
-		beginTime.set(year, month, date, 0, 0);
-		long startMillis = beginTime.getTimeInMillis();
-		Calendar endTime = Calendar.getInstance();
-		endTime.set(year, month, date, 23, 59);
-		long endMillis = endTime.getTimeInMillis();
-
-		ContentResolver cr = getContentResolver();
-
-		// Construct the query with the desired date range.
-		Uri.Builder builder = Instances.CONTENT_URI.buildUpon();
-		ContentUris.appendId(builder, startMillis);
-		ContentUris.appendId(builder, endMillis);
-		String selection;
-		String[] selectionArgs;
-		CheckBox cbShowAll = (CheckBox) findViewById(R.id.cbShowAll);
-		if (cbShowAll.isChecked()) {
-			selection = null;
-			selectionArgs = null;
-		} else {
-			selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
-					+ Calendars.ACCOUNT_TYPE + " = ?) AND ("
-					+ Calendars.OWNER_ACCOUNT + " = ?))";
-			selectionArgs = new String[] { calName, calType, calOwner };
-
-			Log.i(DEBUG_TAG, "Don't show all calendars!");
-		}
-
-		// Submit the query
-		cur = cr.query(builder.build(), INSTANCE_PROJECTION, selection,
-				selectionArgs, null);
-		cur.moveToFirst();
-
-		// Go through Calendar events and print them
-		while (cur.moveToNext()) {
-
-			long calID = 0;
-			long beginVal = 0;
-			long endVal = 0;
-			String title = null;
-			String description = null;
-
-			// Get the field values
-			calID = cur.getLong(PROJECTION_ID_INDEX);
-			beginVal = cur.getLong(PROJECTION_BEGIN_INDEX);
-			endVal = cur.getLong(PROJECTION_END_INDEX);
-			title = cur.getString(PROJECTION_TITLE_INDEX);
-			description = cur.getString(PROJECTION_DESCRIPTION_INDEX);
-
-			Log.w(DEBUG_TAG, "CalID1: " + calID);
-
-			Calendar calBegin = Calendar.getInstance();
-			calBegin.setTimeInMillis(beginVal);
-			Calendar calEnd = Calendar.getInstance();
-			calEnd.setTimeInMillis(endVal);
-
-			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-			String eventStart = formatter.format(calBegin.getTime());
-			String eventEnd = formatter.format(calEnd.getTime());
-
-			Log.i(DEBUG_TAG, title + ": " + eventStart + " - " + eventEnd + " "
-					+ description);
-
-			// Write events to TextView, one event per line
-			details = title + ": " + eventStart + " - " + eventEnd + ":  "
-					+ description;
-			sb.append(details + "\n");
-			calData.setText(sb.toString());
-
-		}
-	}
 }
